c02c1350e1cb7449c105f7c4f80ddb9e87518057
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-11-28 13:28:57 +0900

    Update classic-sg-allow-cis.tf

--- classic-sg-allow-cis.tf
+++ classic-sg-allow-cis.tf
@@ -1,1 +1,12 @@
+data "http" "cis_ips" {
+  url = "https://api.cis.cloud.ibm.com/v1/ips"
 
+  # Optional request headers
+  request_headers {
+    "Accept" = "application/json"
+  }
+}
+
+output "cis_ips_key" {
+  value = "${jsondecode(data.http.cis_ips.body)["result"]}"
+}
