1d6a7b3c08c2da80cf283a5a85b3a1e20b79dde9
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-11-29 00:54:53 +0900

    Update classic-sg-allow-cis.tf

--- classic-sg-allow-cis.tf
+++ classic-sg-allow-cis.tf
@@ -1,11 +1,23 @@
-data "external" "cis_ips" {
-  program = ["bash", "get_cis_ips.sh"]
+data "external" "cis_ipv4_cidrs" {
+  program = ["bash", "get_cis_ipv4_cidrs.sh"]
 }
 
-output "cis_ipv4_cidrs" {
-  value = "${ length( data.external.cis_ips.result ) }"
+data "external" "cis_ipv6_cidrs" {
+  program = ["bash", "get_cis_ipv6_cidrs.sh"]
 }
 
-output "cis_ipv4_cidrs_2" {
-  value = "${ lookup( data.external.cis_ips.result, 2 ) }"
+output "test1" {
+  value = "${ length( data.external.cis_ipv4_cidrs.result ) }"
+}
+
+output "test2" {
+  value = "${ lookup( data.external.cis_ipv4_cidrs.result, 2 ) }"
+}
+
+output "test3" {
+  value = "${ length( data.external.cis_ipv6_cidrs.result ) }"
+}
+
+output "test4" {
+  value = "${ lookup( data.external.cis_ipv6_cidrs.result, 2 ) }"
 }
