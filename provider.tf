99b62af3c010cad13ce8d75c78887b8c9885afd7
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-11-29 00:38:43 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -10,3 +10,7 @@ provider "ibm" {
   iaas_classic_username = "${var.iaas_classic_username}"
   iaas_classic_api_key  = "${var.iaas_classic_api_key}"
 }
+
+terraform {
+  required_version = "~> 0.12"
+}
