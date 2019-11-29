deba8ae017c7c39bc8541c7fea55f53db068e7ba
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-11-29 15:04:20 +0900

    Update fn_apply_cis_ips.tf

--- fn_apply_cis_ips.tf
+++ fn_apply_cis_ips.tf
@@ -5,4 +5,17 @@ resource "ibm_function_action" "apply_cis_ips" {
     kind = "khayama/ibmcloud-cli"
     image = "${file("apply_cis_ips.sh")}"
   }
+  
+  user_defined_parameters = <<EOF
+        [
+    {
+        "key":"IBMCLOUD_API_KEY",
+        "value":""
+    }
+    {
+        "key":"WORKSPACE_ID",
+        "value":"khayama-classic-sg-allow-cis-b3c0d072-955c-4d"
+    }
+        ]
+        EOF
 }
