112a53f23711d9bb44a9148de132946b9e0ba772
Kyouhei Hayama <khayama@users.noreply.github.com>
2019-11-29 15:00:20 +0900

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -7,6 +7,7 @@ variable "iaas_classic_api_key" {
 }
 
 provider "ibm" {
+  region = "jp-tok"
   iaas_classic_username = "${var.iaas_classic_username}"
   iaas_classic_api_key  = "${var.iaas_classic_api_key}"
 }
